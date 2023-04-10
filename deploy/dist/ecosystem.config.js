af912b7ed1239d33314ce0caa07812c43d133f38
Julien Viatgé <julien.viatge@gmail.com>
2023-04-10 11:24:20 +0200

    add pm2 server manage

---
+++ deploy/dist/ecosystem.config.js
@@ -0,0 +1,38 @@
+"use strict";
+const HOST = "217.182.170.219";
+const USER = "ubuntu";
+const REPOSITORY = "jviatge/foodz";
+const PATH = "/home/ubuntu/www/foodz";
+const PACKAGE_MANAGER = "pnpm";
+const REF = "origin/main";
+const NAME_PROJECT_SNAKE = "foodz";
+/*const gitURl = `https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${PROJECT_GIT}`*/
+/*https://@github.com/jviatge/foodz*/
+const start = (type, env) => `cd ./server && pm2 startOrRestart ecosystem.config.js --env ${env} --only ${NAME_PROJECT_SNAKE}_${type} && cd ../`;
+
+module.exports = {
+    apps: [{
+            cwd: "../../",
+            script: PACKAGE_MANAGER,
+            args: "start",
+            autorestart: true,
+        }],
+    // Deployment Configuration
+    deploy: {
+        production: {
+            "user": USER,
+            "host": [HOST],
+            "ref": REF,
+            "repo": `https://@github.com/${REPOSITORY}`,
+            "path": PATH,
+            "pre-deploy-local": "",
+            "pre-deploy": `${PACKAGE_MANAGER} install && 
+				cd server && 
+				${PACKAGE_MANAGER} install && 
+				${PACKAGE_MANAGER} run build`,
+            "post-deploy": `${start("production", "production")} &&
+				pm2 save`,
+            "pre-setup": "",
+        }
+    }
+};
