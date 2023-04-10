b7f3c52b0349f72a37ca315e017260bd899f722b
Julien Viatgé <julien.viatge@gmail.com>
2023-04-10 11:09:37 +0200

    add pm2 server manage

--- deploy/src/ecosystem.config.ts
+++ deploy/src/ecosystem.config.ts
@@ -3,10 +3,14 @@ const USER = "ubuntu"
 const REPOSITORY = "jviatge/foodz"
 const PATH = "/home/ubuntu/www/foodz"
 const PACKAGE_MANAGER = "pnpm"
+const REF = "origin/main"
+const NAME_PROJECT_SNAKE = "foodz"
 
 /*const gitURl = `https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${PROJECT_GIT}`*/
 
 /*https://@github.com/jviatge/foodz*/
+
+const start = (type:string, env:string) => `cd ./server && pm2 startOrRestart ecosystem.config.js --env ${env} --only ${NAME_PROJECT_SNAKE}_${type} && cd ../`
 
 module.exports = {
 
@@ -22,18 +26,21 @@ module.exports = {
 		production : {
 			"user" : USER,
 			"host" : [HOST],
-			"ref"  : "origin/main",
+			"ref"  : REF,
 			/*"repo" : `git@github.com:${REPOSITORY}.git`,*/
 			"repo" : `https://@github.com/${REPOSITORY}`,
 			"path" : PATH,
 
 			"pre-deploy-local" : "",
-			"post-deploy" :
-				`${PACKAGE_MANAGER} install && 
+			"pre-deploy" :  `${PACKAGE_MANAGER} install && 
 				cd server && 
 				${PACKAGE_MANAGER} install && 
-				${PACKAGE_MANAGER} run build `,
+				${PACKAGE_MANAGER} run build`,
+			"post-deploy" :
+				`${start("production", "production")} &&
+				pm2 save'`,
 			"pre-setup" : "",
 		}
 	}
 };
+
